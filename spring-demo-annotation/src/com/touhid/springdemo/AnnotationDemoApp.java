49ab373857a90fbaab1bb021c329e3bfe98612b8
touhid-jisan <touhidulislamjisan@gmail.com>
2020-08-28 01:28:24 +0600

    Annotation - Default Component Names

--- spring-demo-annotation/src/com/touhid/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotation/src/com/touhid/springdemo/AnnotationDemoApp.java
@@ -9,7 +9,8 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring contatiner
-		Coach theCoach = context.getBean("thatSillyCoach" , Coach.class); 
+		//Coach theCoach = context.getBean("thatSillyCoach" , Coach.class); 
+		Coach theCoach = context.getBean("tennisCoach" , Coach.class); 
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
--- spring-demo-annotation/src/com/touhid/springdemo/TennisCoach.java
+++ spring-demo-annotation/src/com/touhid/springdemo/TennisCoach.java
@@ -2,7 +2,8 @@ package com.touhid.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+//@Component("thatSillyCoach")
+@Component // default bean id is "tennisCoach"
 public class TennisCoach implements Coach {
 
 	@Override
