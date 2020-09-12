81e81005e482b0a9390c3ec65868295371305db9
touhid-jisan <touhidulislamjisan@gmail.com>
2020-09-12 07:48:48 +0600

    AOP Pointcut Declarations - Combining Pointcuts

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/touhid/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.touhid.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.touhid.aopdemo")
+public class DemoConfig {
+	
+}
--- spring-demo-aop-pointcut-declarations-combo/src/com/touhid/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations-combo/src/com/touhid/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,15 +11,32 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
 	@Pointcut("execution(* com.touhid.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-	
+	private void forDaoPackage() {
+	}
+
+	// pointcut for getter methods
+	@Pointcut("execution(* com.touhid.aopdemo.dao.*.get*(..))")
+	private void getter() {
+	}
+
+	// pointcut for setter methods
+	@Pointcut("execution(* com.touhid.aopdemo.dao.*.set*(..))")
+	private void setter() {
+	}
+
+	// pointcut for including package ... exclude getter / setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void noSetterNogetter() {
+	}
+
 	@Before("forDaoPackage()") // matches with any parameter
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====> Performing Add account advice");
 	}
-	
-	@Before("forDaoPackage()")
+
+	@Before("noSetterNogetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n=====> Performing API Analytics");
 	}
+
 }
